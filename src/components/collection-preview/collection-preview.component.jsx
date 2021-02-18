83eced94aa2a04e1e37f62148ce65512f24fd44b
Richard Phoon <richardphoon121@gmail.com>
2021-02-18 17:43:35 +0800

    moved directory and shop data into reducers, created redux flow for directory and shop

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,7 +4,7 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({title, items, routeName}) => {
+const CollectionPreview = ({title, items}) => {
     return (
         <div className='collection-preview'>
             <h1 className='title'>{title.toUpperCase()}</h1>
