ab86410a0fa13d6174c752b6f668420cb3712568
Richard Phoon <richardphoon121@gmail.com>
2021-07-04 12:06:52 +0800

    separating client and server code

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,21 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51IMVIuE6lJylfMqslLi04wTfpHqi4MXJ6E6CSAPraxmcYuwBJlxLEJc5i6MaauN9GfttOnTbVaQXT9YuF3nIx6hc005XXC47yd";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert("There was an issue.");
+      });
   };
 
   return (
