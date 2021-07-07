82bfed2724fe6a1da71b7cae903cdf1ad5a9e8ac
Richard Phoon <richardphoon121@gmail.com>
2021-07-07 10:42:55 +0800

    remove crown image from stripe checkout

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -31,7 +31,6 @@ const StripeCheckoutButton = ({ price }) => {
       name="CRWN Clothing Ltd."
       billingAddress
       shippingAddress
-      image="https://sendeyo.com/en/f3eb2117da"
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
